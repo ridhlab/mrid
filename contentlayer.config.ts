4d6028688fb217660584cb3db5f140e4a74c6e23
ridhlab <mohridwan1707@gmail.com>
2024-01-23 07:44:19 +0700

    feat(contentlayer): add pretty code

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,8 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 export const Post = defineDocumentType(() => ({
     name: "Post",
@@ -36,4 +40,38 @@ export const Post = defineDocumentType(() => ({
 export default makeSource({
     contentDirPath: "src/contents",
     documentTypes: [Post],
+    mdx: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [
+            rehypeSlug,
+            [
+                rehypePrettyCode as unknown as any,
+                {
+                    theme: "material-theme-ocean",
+                    onVisitLine(node: any) {
+                        // Prevent lines from collapsing in `display: grid` mode, and allow empty
+                        // lines to be copy/pasted
+                        if (node.children.length === 0) {
+                            node.children = [{ type: "text", value: " " }];
+                        }
+                    },
+                    onVisitHighlightedLine(node: any) {
+                        node.properties.className.push("line--highlighted");
+                    },
+                    onVisitHighlightedWord(node: any) {
+                        node.properties.className = ["word--highlighted"];
+                    },
+                },
+            ],
+            [
+                rehypeAutolinkHeadings,
+                {
+                    properties: {
+                        className: ["subheading-anchor"],
+                        ariaLabel: "Link to section",
+                    },
+                },
+            ],
+        ],
+    },
 });
